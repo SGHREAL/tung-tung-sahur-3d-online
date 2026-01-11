4d06f8c8a14b7b04f01c46d91cd9d54f655a991b
SGHREAL <peacelovepawsnj@gmail.com>
2026-01-10 20:13:28 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,19 @@
-
+let mySprite = sprites.create(img`
+    . . f f f f f f . . 
+    . . f 4 4 4 4 f . . 
+    . . f f 4 4 f f . . 
+    . . f 4 4 4 4 f . . 
+    . . f f f f f f . . 
+    . . f f f f f f . . 
+    f f f c c c c f f f 
+    f f f c c c c f f f 
+    f f f c c c c f f f 
+    f f f c c c c f f f 
+    f f f c c c c f f f 
+    f f f c c c c f f f 
+    . . f f f f f f . . 
+    . . f f . . f f . . 
+    . . f f . . f f . . 
+    . . f f . . f f . . 
+    . . f f . . f f . . 
+    `, SpriteKind.Player)
